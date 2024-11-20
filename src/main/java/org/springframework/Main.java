ba6d46afdf84eabd941311534f5edaecef88c893
unknown <Yusuv>
2024-11-20 23:08:10 +0300

    second  commit

--- src/main/java/org/springframework/Main.java
+++ src/main/java/org/springframework/Main.java
@@ -6,15 +6,11 @@ public class Main {
     public static void main(String[] args) {
         int value = new Scanner(System.in).nextInt();
 
-
         for (int i = 1; i < value + 1; i++){
-
             for (int second_i = 1; second_i < value + 1; second_i ++){
 
                 if (i * second_i == value){
-
                 System.out.println(i + " * " + second_i);
-
                 }
             }
         }
