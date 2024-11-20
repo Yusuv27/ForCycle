ba0ea5cfab06b178a5ef6f3273065c5ae8682a5c
unknown <Yusuv>
2024-11-20 23:07:21 +0300

    first commit

---
+++ src/main/java/org/springframework/Main.java
@@ -0,0 +1,22 @@
+package org.springframework;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value = new Scanner(System.in).nextInt();
+
+
+        for (int i = 1; i < value + 1; i++){
+
+            for (int second_i = 1; second_i < value + 1; second_i ++){
+
+                if (i * second_i == value){
+
+                System.out.println(i + " * " + second_i);
+
+                }
+            }
+        }
+    }
+}
